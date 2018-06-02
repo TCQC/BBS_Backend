96d2dd4581835a5ceb2690010c353928a6ada031
tuanz1 <670779246@qq.com>
2018-06-02 11:18:51 +0800

    build basic project

--- src/main/java/com/tcqc/bbs/BbsApplication.java
+++ src/main/java/com/tcqc/bbs/BbsApplication.java
@@ -1,8 +1,10 @@
 package com.tcqc.bbs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.tuanzi.bbs.dao")
 @SpringBootApplication
 public class BbsApplication {
 
