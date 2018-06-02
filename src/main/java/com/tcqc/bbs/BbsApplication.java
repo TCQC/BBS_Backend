1f0cf25319482b5b02bb85d88e44dc237ceb4882
CHGSM <863159411@qq.com>
2018-06-02 08:56:29 +0800

    build project

---
+++ src/main/java/com/tcqc/bbs/BbsApplication.java
@@ -0,0 +1,12 @@
+package com.tcqc.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsApplication.class, args);
+    }
+}
